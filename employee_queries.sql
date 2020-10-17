808f597ebc6fbdd371150cbf2acfe6272b1238f9
cmoeser5 <carolineannmoeser@gmail.com>
2020-10-17 23:06:56 +0000

    add queries

--- employee_queries.sql
+++ employee_queries.sql
@@ -1,4 +1,5 @@
--- List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- List the following details of each employee: 
+-- employee number, last name, first name, sex, and salary.
 
 SELECT emp.emp_no as employee_number, emp.last_name, emp.first_name, emp.sex, sal.salary
 FROM employees as emp
@@ -18,3 +19,18 @@ SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM departments d 
 JOIN dept_manager dm ON (d.dept_no = dm.dept_no)
 JOIN employees e ON (dm.emp_no = e.emp_no);
+
+-- List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON (e.emp_no = de.emp_no)
+JOIN departments d ON (de.dept_no = d.dept_no);
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name, sex
+FROM employees 
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%'
