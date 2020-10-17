96b595bbd50bdf16d81fdd69f451e20c8d612a12
cmoeser5 <carolineannmoeser@gmail.com>
2020-10-17 23:15:22 +0000

    add queries

--- employee_queries.sql
+++ employee_queries.sql
@@ -3,14 +3,16 @@
 
 SELECT emp.emp_no as employee_number, emp.last_name, emp.first_name, emp.sex, sal.salary
 FROM employees as emp
-*LEFT* JOIN salaries as sal
+LEFT JOIN salaries as sal
 ON emp.emp_no = sal.emp_no
 ORDER BY emp.emp_no;
+
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
 
 -- List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name.
@@ -20,6 +22,7 @@ FROM departments d
 JOIN dept_manager dm ON (d.dept_no = dm.dept_no)
 JOIN employees e ON (dm.emp_no = e.emp_no);
 
+
 -- List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
 
@@ -28,9 +31,29 @@ FROM employees e
 JOIN dept_emp de ON (e.emp_no = de.emp_no)
 JOIN departments d ON (de.dept_no = d.dept_no);
 
+
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules'
-AND last_name LIKE 'B%'
+AND last_name LIKE 'B%';
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e 
+JOIN dept_emp de ON (e.emp_no = de.emp_no)
+JOIN departments d ON (de.dept_no = d.dept_no)
+WHERE dept_name = 'Sales';
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e 
+JOIN dept_emp de ON (e.emp_no = de.emp_no)
+JOIN departments d ON (de.dept_no = d.dept_no)
+WHERE dept_name = 'Sales' OR dept_name = 'Development'
+
